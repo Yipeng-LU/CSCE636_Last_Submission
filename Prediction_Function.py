6e35161bd6d1a22b3d97747e63fa577a816120b5
Yipeng-LU <60021040+Yipeng-LU@users.noreply.github.com>
2020-04-29 13:47:05 -0500

    Update Prediction_Function.py

--- Prediction_Function.py
+++ Prediction_Function.py
@@ -44,7 +44,7 @@ inp=np.ndarray((len(names),20,50), dtype='float32')
 for i,v in enumerate(names):
   os.mkdir('/content/temp/frameFor_{}/'.format(v))
   os.system('cd openpose')
-  os.system('./build/examples/openpose/openpose.bin --video $root2$v --write_json '/content/temp/frameFor_$v/' --display 0 --render_pose 0')
+  os.system('./build/examples/openpose/openpose.bin --video $root2$v --write_json /content/temp/frameFor_$v/ --display 0 --render_pose 0')
   root3='/content/temp/frameFor_{}/'.format(v)
   ls=os.listdir(root3)
   ls.sort()
